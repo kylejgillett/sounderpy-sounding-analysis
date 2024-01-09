3255e1670f934242e4e60746b0fc86e397cc0171
kjgillett10@gmail.com <kjgillett10@gmail.com>
2024-01-09 19:34:32 +0000

    Created example-sounding_dark.png, 251580013-2e9477c9-e36a-4163-accb-fe46780058dd.png and GDSrWo0XkAAGWX5-logo.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -11,3 +12,17 @@ class Form1(Form1Template):
   def station_id_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     pass
+
+  def obs_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    params = anvil.server.call('get_obs_sounding',
+                               self.station_id.text,
+                               self.get_date.date.strftime('%Y'),
+                               self.get_date.date.strftime('%m'),
+                               self.get_date.date.strftime('%d'),
+                               self.launch_hour.text,
+                               self.dark_mode_check.checked,
+                               self.color_blind_check.checked)
+    self.image_sounding.source = params[0]
+    self.plot_area_label.text = params[1]
+    
