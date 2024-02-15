5e65a73874c57984fc472738630e567c8017156b
kjgillett10@gmail.com <kjgillett10@gmail.com>
2024-02-15 22:31:48 +0000

    Created Form2; Edited get_soundings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -62,6 +62,24 @@ class Form1(Form1Template):
     self.bufkit_image_display.source = params[0]
     self.bufkit_plot_label.text = params[1]
 
+
+
+  def reanl_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.reanl_standby_label.text = "YOUR REQUEST IS PROCESSING, THIS MAY TAKE A MOMENT..."
+    params = anvil.server.call('get_reanl_sounding',
+                               [float(self.reanl_lat.text), float(self.reanl_lon.text)],
+                               self.reanl_date.date.strftime('%Y'),
+                               self.reanl_date.date.strftime('%m'),
+                               self.reanl_date.date.strftime('%d'),
+                               self.reanl_hour.text,
+                               self.reanl_dark_mode_check.checked,
+                               self.reanl_color_blind_check.checked)
+    self.reanl_image_display.source = params[0]
+    self.reanl_plot_label.text = params[1]
+
+
+
   # def timer_1_tick(self, **event_args):
   #   """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
   #   image = anvil.server.call('timer_task')
--- server_code/get_soundings.py
+++ server_code/get_soundings.py
@@ -1,14 +1,183 @@
 import anvil.server
+import sounderpy as spy
+import anvil.media
+from datetime import datetime as dt
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+import threading
+import queue
+
+# Lock to ensure thread-safe access to shared resources
+lock = threading.Lock()
+
+# Queue to store tasks (requests)
+task_queue = queue.Queue()
+
+def worker():
+    while True:
+        # Get a task from the queue
+        task = task_queue.get()
+
+        # Exit the loop if a sentinel value is received (indicating the worker should stop)
+        if task is None:
+            break
+
+        # Process the task based on the function name
+        if task["function"] == "raob_function":
+            result = process_raob_function(*task["args"])
+
+        elif task["function"] == "acars_list_function":
+            result = process_acars_list_function(*task["args"])
+
+        elif task["function"] == "acars_function":
+            result = process_acars_function(*task["args"])
+
+        elif task["function"] == "bufkit_function":
+            result = process_bufkit_function(*task["args"])
+
+        elif task["function"] == "reanl_function":
+            result = process_reanl_function(*task["args"])
+
+        # Mark the task as done
+        task_queue.task_done()
+
+# Start a worker thread
+worker_thread = threading.Thread(target=worker)
+worker_thread.start()
+
+
+
+
+# Example functions that may require different variables
+def process_raob_function(site_id, year, month, day, hour, color_blind, dark_mode):
+    with lock:
+        clean_data = spy.get_obs_data(str(site_id), str(year), str(month), str(day), str(hour))
+
+        label_txt = f"{clean_data['site_info']['valid-time'][3]}Z Launch for {clean_data['site_info']['site-id']}, {clean_data['site_info']['site-name']} at {clean_data['site_info']['valid-time'][1]}-{clean_data['site_info']['valid-time'][2]}-{clean_data['site_info']['valid-time'][0]}-{clean_data['site_info']['valid-time'][3]}Z"
+        spy.build_sounding(clean_data, dark_mode=dark_mode, color_blind=color_blind, save=True)
+
+        image = anvil.media.from_file('sounderpy_sounding.png', 'image/jpeg')
+
+        print(dt.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+
+        return image, label_txt
+
+def process_acars_list_function(year, month, day, hour):
+    with lock:
+
+        profiles_list = spy.acars_data(str(year), str(month), str(day), str(hour)).list_profiles()
+
+        print(dt.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+
+        return profiles_list
+
+def process_acars_function(profile_id, year, month, day, hour, color_blind, dark_mode):
+    with lock:
+        clean_data = spy.acars_data(str(year), str(month), str(day), str(hour)).get_profile(profile_id)
+
+        label_txt = f"{clean_data['site_info']['valid-time'][3]}Z Launch for {clean_data['site_info']['site-id']}, {clean_data['site_info']['site-name']} at {clean_data['site_info']['valid-time'][1]}-{clean_data['site_info']['valid-time'][2]}-{clean_data['site_info']['valid-time'][0]}-{clean_data['site_info']['valid-time'][3]}Z"
+
+        spy.build_sounding(clean_data, dark_mode=dark_mode, color_blind=color_blind, save=True)
+
+        image =anvil.media.from_file('sounderpy_sounding.png', 'image/jpeg')
+
+        print(dt.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+
+        return image, label_txt
+
+def process_bufkit_function(model, bufkit_site, fcst_hr, color_blind, dark_mode):
+    with lock:
+        clean_data = spy.get_bufkit_data(str(model), str(bufkit_site), int(fcst_hr))
+
+        label_txt = f"{clean_data['site_info']['valid-time'][3]}Z Launch for {clean_data['site_info']['site-id']}, {clean_data['site_info']['site-name']} at {clean_data['site_info']['valid-time'][1]}-{clean_data['site_info']['valid-time'][2]}-{clean_data['site_info']['valid-time'][0]}-{clean_data['site_info']['valid-time'][3]}Z"
+
+        spy.build_sounding(clean_data, dark_mode=dark_mode, color_blind=color_blind, save=True)
+
+        image = anvil.media.from_file('sounderpy_sounding.png', 'image/jpeg')
+
+        print(dt.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+
+        return image, label_txt
+
+
+def process_reanl_function(latlon, year, month, day, hour, color_blind, dark_mode):
+    with lock:
+        clean_data = spy.get_model_data('rap-ruc', latlon, str(year), str(month), str(day), str(hour))
+
+        label_txt = f"{clean_data['site_info']['valid-time'][3]}Z  Reanalysis for {clean_data['site_info']['site-latlon']} at {clean_data['site_info']['valid-time'][1]}-{clean_data['site_info']['valid-time'][2]}-{clean_data['site_info']['valid-time'][0]}-{clean_data['site_info']['valid-time'][3]}Z"
+
+        spy.build_sounding(clean_data, dark_mode=dark_mode, color_blind=color_blind, save=True)
+
+        image = anvil.media.from_file('sounderpy_sounding.png', 'image/jpeg')
+
+        print(dt.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+
+        return image, label_txt
+
+
+
+#####################
+# GET RAOB FUNCTION #
+#####################
+@anvil.server.callable
+def get_raob_sounding(site_id, year, month, day, hour, color_blind, dark_mode):
+    # Add the task (request) to the queue
+    task_queue.put({"function": "raob_function", "args": (site_id, year, month, day, hour, color_blind, dark_mode)})
+
+    image, label_txt = process_raob_function(site_id, year, month, day, hour, color_blind, dark_mode)
+
+    return image, label_txt
+
+
+###########################
+# GET ACARS LIST FUNCTION #
+###########################
+@anvil.server.callable
+def get_acars_profile_list(year, month, day, hour):
+    # Add the task (request) to the queue
+    task_queue.put({"function": "acars_list_function", "args": (year, month, day, hour)})
+
+    acars_list = process_acars_list_function(year, month, day, hour)
+
+    return acars_list
+
+
+######################
+# GET ACARS FUNCTION #
+######################
+@anvil.server.callable
+def get_acars_sounding(profile_id, year, month, day, hour, color_blind, dark_mode):
+    # Add the task (request) to the queue
+    task_queue.put({"function": "acars_function", "args": (profile_id, year, month, day, hour, color_blind, dark_mode)})
+
+    image, label_txt = process_acars_function(profile_id, year, month, day, hour, color_blind, dark_mode)
+
+    return image, label_txt
+
+
+#######################
+# GET BUFKIT FUNCTION #
+#######################
+@anvil.server.callable
+def get_bufkit_sounding(model, bufkit_site, fcst_hr, color_blind, dark_mode):
+    # Add the task (request) to the queue
+    task_queue.put({"function": "bufkit_function", "args": (model, bufkit_site, fcst_hr, color_blind, dark_mode)})
+
+    image, label_txt = process_bufkit_function(model, bufkit_site, fcst_hr, color_blind, dark_mode)
+
+    return image, label_txt
+
+
+#######################
+# GET REANL FUNCTION #
+#######################
+@anvil.server.callable
+def get_reanl_sounding(latlon, year, month, day, hour, color_blind, dark_mode):
+    # Add the task (request) to the queue
+    task_queue.put({"function": "reanl_function", "args": (latlon, year, month, day, hour, color_blind, dark_mode)})
+
+    image, label_txt = process_reanl_function(latlon, year, month, day, hour, color_blind, dark_mode)
+
+    return image, label_txt
