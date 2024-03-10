4d8938c27672b57607bf2de1d89e86a20414d447
kjgillett10@gmail.com <kjgillett10@gmail.com>
2024-03-10 01:36:28 +0000

    Created HOME.raob_map_comp; Edited HOME.raob_form, HOME.about_form and HOME.bufkit_form

---
+++ client_code/HOME/raob_map_comp/__init__.py
@@ -0,0 +1,10 @@
+from ._anvil_designer import raob_map_compTemplate
+from anvil import *
+import anvil.server
+
+class raob_map_comp(raob_map_compTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
