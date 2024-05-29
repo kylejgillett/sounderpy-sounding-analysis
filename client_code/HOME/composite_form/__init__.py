0b7a7d86feea3dee3175d03ecbf87943636396fd
kjgillett10@gmail.com <kjgillett10@gmail.com>
2024-05-29 12:29:09 +0000

    Created HOME.composite_form.fcst_forecast_hr; Edited HOME.composite_form

--- client_code/HOME/composite_form/__init__.py
+++ client_code/HOME/composite_form/__init__.py
@@ -2,10 +2,15 @@ from ._anvil_designer import composite_formTemplate
 from anvil import *
 import anvil.server
 
+from fcst_forecast_hr import fcst_forecast_hr
 
 class composite_form(composite_formTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+      def raob_tab_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.tab_panel.clear()
+        self.tab_panel.add_component(raob_form())
+        pass
