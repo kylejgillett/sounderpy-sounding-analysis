d499740db823c1efd4f662fda7c8b9c78d593039
kjgillett10@gmail.com <kjgillett10@gmail.com>
2024-12-09 12:22:26 +0000

    Edited HOME, HOME.composite_form and HOME.composite_form.Form1

--- client_code/HOME/composite_form/Form1/__init__.py
+++ client_code/HOME/composite_form/Form1/__init__.py
@@ -2,6 +2,8 @@ from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 
+from ..bufkit_fcsthrs import bufkit_fcsthrs
+
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -9,3 +11,10 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+    
+  def fcst_hours_tab_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.tab_panel.clear()
+    self.tab_panel.add_component(raob_form())
+    pass
